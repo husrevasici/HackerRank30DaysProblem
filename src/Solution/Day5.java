f05d815818fa746859dbd61b5f9d9cf6d122efb0
husrevasici <mullahusrevasici@gmail.com>
2022-01-04 21:46:55 +0300

    5th day - problem added

---
+++ src/Solution/Day5.java
@@ -0,0 +1,27 @@
+package Solution;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Day5 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+		for (int j = 1; j < 11; j++) {
+			System.out.println(n + " x " + j + " = " + n * j);
+		}
+
+		bufferedReader.close();
+	}
+}
